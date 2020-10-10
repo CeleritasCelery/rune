abb5da3cdf380f0f655475488b09e25b23a0fae0
Mac Man <t.macman@gmail.com>
2020-10-09 20:03:18 -0600

    Clean up parse code and add tests

--- src/parse.rs
+++ src/parse.rs
@@ -1,40 +1,34 @@
-struct CharIndex {
-    front_offset: usize,
-    char: char,
+use std::str;
+use std::slice;
+
+trait PrevIter {
+    fn prev(&mut self);
 }
 
-struct ParseIter<'a> {
-    next: Option<(usize, char)>,
-    iter: std::str::CharIndices<'a>,
-}
-
-impl<'a> Iterator for ParseIter<'a> {
-    type Item = (usize, char);
-
-    #[inline]
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.next {
-            Some(i) => {
-                let item = i;
-                self.next = None;
-                Some(item)
-            }
-            None => self.iter.next()
-        }
-    }
-}
-
-impl<'a> ParseIter<'a> {
-    fn put_back(&mut self, char_index: (usize, char)) {
-        self.next = Some(char_index);
+impl PrevIter for str::Chars<'_> {
+    fn prev(&mut self) {
+        let orig_str = self.as_str();
+        let orig_ptr = orig_str.as_ptr();
+        let char_len = unsafe {
+            const MAX_UTF8_CHARS: usize = 4;
+            let tmp_ptr = orig_ptr.sub(MAX_UTF8_CHARS);
+            let tmp_slice = slice::from_raw_parts(tmp_ptr, MAX_UTF8_CHARS);
+            let mut iter = str::from_utf8_unchecked(tmp_slice).chars();
+            iter.next_back();
+            MAX_UTF8_CHARS - iter.as_str().len()
+        };
+        *self = unsafe {
+            let new_len = orig_str.len() + char_len;
+            let new_slice = slice::from_raw_parts(orig_ptr.sub(char_len), new_len);
+            str::from_utf8_unchecked(new_slice).chars()
+        };
     }
 }
 
 pub fn parse(sexp: &str) -> Vec<String> {
-    let mut chars = ParseIter{next: None, iter: sexp.char_indices()};
+    let mut chars = sexp.chars();
     let mut lexems: Vec<String> = Vec::new();
-    while let Some(char_index) = chars.next() {
-        let char = char_index.1;
+    while let Some(char) = chars.next() {
         match char {
             '(' | ')' => {
                 lexems.push(char.to_string());
@@ -42,13 +36,18 @@ pub fn parse(sexp: &str) -> Vec<String> {
             ' ' | '\t' => {
                 lexems.push("space".to_string());
             }
+            '\'' => {
+                lexems.push("quote".to_string());
+            }
             _   => {
-                chars.put_back(char_index);
+                chars.prev();
                 // TODO: unchecked
-                let symbol: String = parse_symbol(&sexp, &mut chars);
+                let symbol = parse_symbol(&mut chars);
+                println!("\"{}\"", symbol);
                 lexems.push(symbol);
             }
         }
+        println!("\"{}\"", chars.as_str());
     }
     return lexems;
 }
@@ -63,29 +62,53 @@ fn symbol_char(char: char) -> bool {
     }
 }
 
+unsafe fn string_from_ptrs(beg: *const u8, end: *const u8) -> String {
+    let len = end as usize - beg as usize;
+    let slice = std::slice::from_raw_parts(beg, len);
+    str::from_utf8_unchecked(slice).to_owned()
+}
 
-fn parse_symbol(sexp: &str, chars: &mut ParseIter) -> String {
+fn parse_symbol(chars: &mut str::Chars) -> String {
     let mut escaped = false;
-    let beg = chars.next().unwrap().0;
+    let beg_str = chars.as_str();
 
-    while let Some(char_index) = chars.next() {
-        let char = char_index.1;
+    while let Some(char) = chars.next() {
         if escaped || char == '\\' {
             escaped = !escaped;
         } else if !symbol_char(char) {
-            chars.put_back(char_index);
-            let end = char_index.0;
             unsafe {
-                return sexp.get_unchecked(beg..end).to_string();
+                chars.prev();
+                let end = chars.as_str().as_ptr();
+                return string_from_ptrs(beg_str.as_ptr(), end);
             }
         }
     }
-    sexp.get(beg..).unwrap().to_string()
+    beg_str.to_owned()
 }
 
-fn run() {
-    let symbols = parse::parse("(foo (bar) baz 'word) bob");
+pub fn run() {
+    let symbols = parse("(foo (bar) baz 'word) bob");
     for s in symbols {
-        println!("{}", s);
+        println!("\"{}\"", s);
+    }
+}
+
+#[cfg(test)]
+mod test {
+    macro_rules! vec_of_strings {
+        ($($x:expr),*) => (vec![$($x.to_string()),*]);
+    }
+
+    #[test]
+    fn parse() {
+        let symbols = super::parse("(foo (bar) baz 'word) bob");
+
+        let golden = vec_of_strings![
+            "(", "foo", "space", "(", "bar", ")", "space",
+            "baz", "space", "quote", "word", ")", "space",
+            "bob"
+        ];
+
+        assert_eq!(golden, symbols);
     }
 }
