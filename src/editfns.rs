ae7409930483f20c58448425c38c406d52a4e382
Troy Hinckley <t.macman@gmail.com>
2021-12-12 17:00:38 -0700

    Define basic format function
    This will only handle %s, but that should get us pretty far

--- src/editfns.rs
+++ src/editfns.rs
@@ -1,6 +1,7 @@
 use fn_macros::defun;
 
 use crate::object::Object;
+use anyhow::{anyhow, ensure, Result};
 
 #[defun]
 fn message(format_string: &str, args: &[Object]) -> String {
@@ -8,4 +9,47 @@ fn message(format_string: &str, args: &[Object]) -> String {
     format_string.to_owned()
 }
 
-defsubr!(message);
+#[defun]
+fn format(string: &str, objects: &[Object]) -> Result<String> {
+    let mut result = String::new();
+    let mut last_end = 0;
+    let mut iter = objects.iter();
+    for (start, part) in string.match_indices("%s") {
+        result.push_str(&string[last_end..start]);
+        let val = iter
+            .next()
+            .ok_or_else(|| anyhow!("Not enough objects for format string"))?;
+        result.push_str(&format!("{}", val));
+        last_end = start + part.len();
+    }
+    ensure!(
+        iter.next().is_none(),
+        "Too many arguments for format string"
+    );
+    result.push_str(&string[last_end..string.len()]);
+    Ok(result)
+}
+
+defsubr!(message, format);
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_format() {
+        assert_eq!(&format("%s", &[1.into()]).unwrap(), "1");
+        assert_eq!(&format("foo-%s", &[2.into()]).unwrap(), "foo-2");
+        assert_eq!(
+            &format("foo-%s %s", &[3.into(), 4.into()]).unwrap(),
+            "foo-3 4"
+        );
+        assert_eq!(
+            &format("%s", &[(&crate::symbol::sym::FUNCTION).into()]).unwrap(),
+            "function"
+        );
+
+        assert!(&format("%s", &[]).is_err());
+        assert!(&format("%s", &[1.into(), 2.into()]).is_err());
+    }
+}
