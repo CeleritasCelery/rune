c6df61ebf11f66a89fc7887007a81a7532549ab4
Troy Hinckley <t.macman@gmail.com>
2022-07-07 16:25:10 -0500

    Refactor

--- src/core/error.rs
+++ src/core/error.rs
@@ -1,7 +1,5 @@
 use std::fmt::Display;
 
-use super::object::Object;
-
 /// The function or form has the wrong number of arguments.
 #[derive(Debug, PartialEq)]
 pub(crate) struct ArgError {
@@ -10,16 +8,7 @@ pub(crate) struct ArgError {
     name: String,
 }
 
-/// Error provided if object was the wrong type
-#[derive(Debug, PartialEq)]
-pub(crate) struct TypeError {
-    expect: Type,
-    actual: Type,
-    print: String,
-}
-
 impl std::error::Error for ArgError {}
-impl std::error::Error for TypeError {}
 
 impl Display for ArgError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
@@ -35,38 +24,12 @@ impl Display for ArgError {
     }
 }
 
-impl Display for TypeError {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        let Self {
-            expect,
-            actual,
-            print,
-        } = self;
-        write!(f, "expected {expect:?}, found {actual:?}: {print}")
-    }
-}
-
 impl ArgError {
     pub(crate) fn new(expect: u16, actual: u16, name: impl AsRef<str>) -> ArgError {
         Self {
             expect,
             actual,
             name: name.as_ref().to_owned(),
-        }
-    }
-}
-
-impl TypeError {
-    /// Get a type error from an object.
-    pub(crate) fn new<'ob, T>(expect: Type, obj: T) -> Self
-    where
-        T: Into<Object<'ob>>,
-    {
-        let obj = obj.into();
-        Self {
-            expect,
-            actual: obj.get_type(),
-            print: obj.to_string(),
         }
     }
 }
@@ -86,4 +49,40 @@ pub(crate) enum Type {
     Func,
     Number,
     List,
+}
+
+/// Error provided if object was the wrong type
+#[derive(Debug, PartialEq)]
+pub(crate) struct TypeError {
+    expect: Type,
+    actual: Type,
+    print: String,
+}
+
+impl std::error::Error for TypeError {}
+
+impl Display for TypeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        let Self {
+            expect,
+            actual,
+            print,
+        } = self;
+        write!(f, "expected {expect:?}, found {actual:?}: {print}")
+    }
+}
+
+impl TypeError {
+    /// Get a type error from an object.
+    pub(crate) fn new<'ob, T>(expect: Type, obj: T) -> Self
+    where
+        T: Into<super::object::Object<'ob>>,
+    {
+        let obj = obj.into();
+        Self {
+            expect,
+            actual: obj.get_type(),
+            print: obj.to_string(),
+        }
+    }
 }
