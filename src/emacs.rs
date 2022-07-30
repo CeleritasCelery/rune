b9a3c6856ab4fbd6688a9d6ea64d1d2c5c8e4826
Troy Hinckley <t.macman@gmail.com>
2022-07-30 16:29:54 -0600

    Define noninteractive variable

--- src/emacs.rs
+++ src/emacs.rs
@@ -10,5 +10,16 @@ defvar!(SYSTEM_TYPE, "system-type", "darwin");
 defvar!(DUMP_MODE, "dump-mode");
 defvar!(COMMAND_LINE_ARGS, "command-line-args", list!(""));
 defvar!(DEFAULT_DIRECTORY, "default-directory", "");
+defvar!(NONINTERACTIVE, "noninteractive", true);
 
-defsubr!(kill_emacs; VARS => {EMACS_VERSION, SYSTEM_TYPE, DUMP_MODE, COMMAND_LINE_ARGS, DEFAULT_DIRECTORY});
+defsubr!(
+    kill_emacs;
+    VARS => {
+        EMACS_VERSION,
+        SYSTEM_TYPE,
+        DUMP_MODE,
+        COMMAND_LINE_ARGS,
+        DEFAULT_DIRECTORY,
+        NONINTERACTIVE
+    }
+);
