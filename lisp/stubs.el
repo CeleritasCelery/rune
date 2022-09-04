e2f65f97ef1b35d4ec520e70a5f350ec9cdaf14d
Troy Hinckley <t.macman@gmail.com>
2022-09-03 21:30:42 -0600

    Add stub of help-add-fundoc-usage

--- lisp/stubs.el
+++ lisp/stubs.el
@@ -4,3 +4,7 @@
 (defun help-uni-confusable-suggestions (string)
   "stub of function for bootstrapping"
   nil)
+
+(defun help-add-fundoc-usage (docstring arglist)
+  "stub of function for bootstrapping"
+  docstring)
