2d05f2473badff5ada89b2b90481eeafffaab63e
Troy Hinckley <t.macman@gmail.com>
2022-11-15 15:10:03 -0700

    Define print variables

--- src/print.rs
+++ src/print.rs
@@ -8,4 +8,11 @@ fn error_message_string(obj: GcObj) -> String {
     format!("Error: {obj}")
 }
 
-define_symbols!(FUNCS => {error_message_string});
+defvar!(PRINT_LENGTH);
+defvar!(PRINT_LEVEL);
+defvar!(PRINT_ESCAPE_NEWLINES);
+
+define_symbols!(
+    FUNCS => {error_message_string}
+    VARS => {PRINT_LENGTH, PRINT_LEVEL, PRINT_ESCAPE_NEWLINES}
+);
