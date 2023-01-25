637a68c1ed3975abf2339af731850fae804ebb08
Troy Hinckley <t.macman@gmail.com>
2023-01-25 07:27:34 -0700

    Add buffer delete method

--- src/core/object/buffer.rs
+++ src/core/object/buffer.rs
@@ -1,3 +1,5 @@
+use bstr::ByteSlice;
+
 pub(crate) struct Buffer {
     /// The pointer to the start of the buffer.
     storage: Box<[u8]>,
@@ -12,7 +14,7 @@ impl Buffer {
         let storage = {
             let capacity = data.len() + Self::GAP_SIZE;
             let mut storage = Vec::with_capacity(capacity);
-            storage.extend_from_slice(&[0; Self::GAP_SIZE]);
+            storage.resize(Self::GAP_SIZE, 0);
             storage.extend_from_slice(data.as_bytes());
             assert_eq!(storage.len(), capacity);
             storage.into_boxed_slice()
@@ -24,7 +26,7 @@ impl Buffer {
         }
     }
 
-    fn grow(&mut self, slice: &[u8]) {
+    fn grow(&mut self, slice: &str) {
         let new_capacity = {
             let pre_gap = self.gap_start;
             let post_gap = self.storage.len() - self.gap_end;
@@ -35,9 +37,9 @@ impl Buffer {
             // pre-gap
             buffer.extend_from_slice(&self.storage[..self.gap_start]);
             // new text
-            buffer.extend_from_slice(slice);
+            buffer.extend_from_slice(slice.as_bytes());
             // gap
-            buffer.extend_from_slice(&[0; Self::GAP_SIZE]);
+            buffer.resize(buffer.len() + Self::GAP_SIZE, 0);
             // post-gap
             buffer.extend_from_slice(&self.storage[self.gap_end..]);
             buffer.into_boxed_slice()
@@ -55,12 +57,22 @@ impl Buffer {
 
     pub(crate) fn insert_string(&mut self, slice: &str) {
         if (self.gap_end - self.gap_start) < slice.len() {
-            self.grow(slice.as_bytes());
+            self.grow(slice);
         } else {
             let new_slice = &mut self.storage[self.gap_start..(self.gap_start + slice.len())];
             new_slice.copy_from_slice(slice.as_bytes());
             self.gap_start += slice.len();
         }
+    }
+
+    fn delete(&mut self, size: usize) {
+        let string = unsafe { self.storage[..self.gap_start].to_str_unchecked() };
+        let idx = string.len().saturating_sub(size);
+        assert!(
+            string.is_char_boundary(idx),
+            "deletion not on utf8 boundary"
+        );
+        self.gap_start = idx;
     }
 }
 
@@ -99,26 +111,27 @@ mod test {
     }
 
     #[test]
-    fn grow() {
-        let string = "hello";
-        let mut buffer = Buffer::new(string);
-        buffer.grow(&[]);
-        assert_eq!(buffer.storage.len(), string.len() + Buffer::GAP_SIZE);
-        assert_eq!(buffer.gap_end, Buffer::GAP_SIZE);
-        assert_eq!(buffer.gap_start, 0);
+    fn delete() {
+        let world = "world";
+        let hello = "hello ";
+        let mut buffer = Buffer::new(world);
+        buffer.insert_string(hello);
+        buffer.delete(4);
+        assert_eq!(buffer.gap_start, hello.len() - 4);
+        assert_eq!(buffer.gap_end, hello.len() + Buffer::GAP_SIZE);
     }
 
     #[test]
     fn resize() {
-        let hello = "hello";
-        let mut buffer = Buffer::new(hello);
-        let world = " world";
-        buffer.insert_string(world);
+        let world = "world";
+        let hello = "hello ";
+        let mut buffer = Buffer::new(world);
+        buffer.insert_string(hello);
         assert_eq!(
             buffer.storage.len(),
             hello.len() + world.len() + Buffer::GAP_SIZE
         );
-        assert_eq!(buffer.gap_end, world.len() + Buffer::GAP_SIZE);
-        assert_eq!(buffer.gap_start, world.len());
+        assert_eq!(buffer.gap_end, hello.len() + Buffer::GAP_SIZE);
+        assert_eq!(buffer.gap_start, hello.len());
     }
 }
