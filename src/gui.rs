658b4f5f9527273245a50c1cb72233e590067bbc
Troy Hinckley <t.macman@gmail.com>
2023-05-26 12:48:52 -0500

    Add bigger GUI example

--- src/gui.rs
+++ src/gui.rs
@@ -1,17 +1,92 @@
-/// The layer to interact with the GUI
-use druid::widget::Label;
-use druid::{AppLauncher, Widget, WindowDesc};
+// Copyright 2020 The Druid Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-fn build_ui() -> impl Widget<()> {
-    Label::new("Hello world")
+//! An example of various text layout features.
+
+// On Windows platform, don't show a console when opening the app.
+use druid::piet::{PietTextLayoutBuilder, TextStorage as PietTextStorage};
+use druid::text::{Attribute, RichText, TextStorage};
+use druid::widget::prelude::*;
+use druid::widget::{LineBreaking, RawLabel, Scroll};
+use druid::{
+    AppLauncher, Color, Data, FontFamily, FontStyle, FontWeight, Lens,
+    TextAlignment, Widget, WidgetExt, WindowDesc,
+};
+
+const TEXT: &str = r#"Contrary to what we would like to believe, there is no such thing as a structureless group. Any group of people of whatever nature that comes together for any length of time for any purpose will inevitably structure itself in some fashion. The structure may be flexible; it may vary over time; it may evenly or unevenly distribute tasks, power and resources over the members of the group. But it will be formed regardless of the abilities, personalities, or intentions of the people involved. The very fact that we are individuals, with different talents, predispositions, and backgrounds makes this inevitable. Only if we refused to relate or interact on any basis whatsoever could we approximate structurelessness -- and that is not the nature of a human group.
+This means that to strive for a structureless group is as useful, and as deceptive, as to aim at an "objective" news story, "value-free" social science, or a "free" economy. A "laissez faire" group is about as realistic as a "laissez faire" society; the idea becomes a smokescreen for the strong or the lucky to establish unquestioned hegemony over others. This hegemony can be so easily established because the idea of "structurelessness" does not prevent the formation of informal structures, only formal ones. Similarly "laissez faire" philosophy did not prevent the economically powerful from establishing control over wages, prices, and distribution of goods; it only prevented the government from doing so. As long as the structure of the group is informal, the rules of how decisions are made are known only to a few and awareness of power is limited to those who know the rules. Those who do not know the rules and are not chosen for initiation must remain in confusion, or suffer from paranoid delusions that something is happening of which they are not quite aware."#;
+
+const SPACER_SIZE: f64 = 8.0;
+
+#[derive(Clone, Data, Lens)]
+struct AppState {
+    text: RichText,
+    line_break_mode: LineBreaking,
+    alignment: TextAlignment,
+}
+
+//NOTE: we implement these traits for our base data (instead of just lensing
+//into the RichText object, for the label) so that our label controller can
+//have access to the other fields.
+impl PietTextStorage for AppState {
+    fn as_str(&self) -> &str {
+        self.text.as_str()
+    }
+}
+
+impl TextStorage for AppState {
+    fn add_attributes(&self, builder: PietTextLayoutBuilder, env: &Env) -> PietTextLayoutBuilder {
+        self.text.add_attributes(builder, env)
+    }
 }
 
 pub(crate) fn launch() {
-    let main_window = WindowDesc::new(build_ui())
-        .window_size((600.0, 400.0))
-        .title("My first Druid App");
+    // describe the main window
+    let main_window = WindowDesc::new(build_root_widget())
+        .title("rune")
+        .window_size((400.0, 600.0));
 
+    let text = RichText::new(TEXT.into())
+        .with_attribute(0..9, Attribute::text_color(Color::rgb(1.0, 0.2, 0.1)))
+        .with_attribute(0..9, Attribute::size(24.0))
+        .with_attribute(0..9, Attribute::font_family(FontFamily::SERIF))
+        .with_attribute(194..239, Attribute::weight(FontWeight::BOLD))
+        .with_attribute(764.., Attribute::size(12.0))
+        .with_attribute(764.., Attribute::style(FontStyle::Italic));
+
+    // create the initial app state
+    let initial_state = AppState {
+        line_break_mode: LineBreaking::WordWrap,
+        alignment: TextAlignment::default(),
+        text,
+    };
+
+    // start the application
     AppLauncher::with_window(main_window)
-        .launch(())
+        .log_to_console()
+        .launch(initial_state)
         .expect("Failed to launch application");
+}
+
+fn build_root_widget() -> impl Widget<AppState> {
+    Scroll::new(
+        RawLabel::new()
+            .with_text_color(Color::BLACK)
+            .with_line_break_mode(LineBreaking::WordWrap)
+            .background(Color::WHITE)
+            .expand_width()
+            .padding((SPACER_SIZE * 4.0, SPACER_SIZE))
+            .background(Color::grey8(222)),
+    ).vertical()
 }
