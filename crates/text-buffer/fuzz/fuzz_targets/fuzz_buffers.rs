dfaaf6a185e60da1d52f093ce4bf31fe88e58d43
Troy Hinckley <t.macman@gmail.com>
2023-08-15 18:59:12 -0500

    Create helper function to debug fuzzer runs

--- crates/text-buffer/fuzz/fuzz_targets/fuzz_buffers.rs
+++ crates/text-buffer/fuzz/fuzz_targets/fuzz_buffers.rs
@@ -1,7 +1,7 @@
 #![no_main]
 
-use text_buffer::Buffer;
 use libfuzzer_sys::fuzz_target;
+use text_buffer::Buffer;
 
 fuzz_target!(|transactions: Vec<Result<(usize, &str), (usize, usize)>>| {
     let mut buffer = Buffer::new();
@@ -22,3 +22,35 @@ fuzz_target!(|transactions: Vec<Result<(usize, &str), (usize, usize)>>| {
         }
     }
 });
+
+#[allow(dead_code)]
+fn create_repo(transactions: &[Result<(usize, &str), (usize, usize)>]) {
+    let mut buffer = Buffer::new();
+    println!("let mut buffer = Buffer::new();");
+    for trans in transactions {
+        match trans {
+            // Insert
+            Ok((pos, text)) => {
+                let pos = pos % (buffer.len() + 2);
+                println!("buffer.set_cursor({pos});");
+                buffer.set_cursor(pos);
+                println!("buffer.insert({text:?});");
+                buffer.insert(text)
+            }
+            // Delete
+            Err((start, end)) => {
+                let start = start % (buffer.len() + 2);
+                let end = end % (buffer.len() + 2);
+                println!("buffer.delete_range({start}, {end});");
+                buffer.delete_range(start, end)
+            }
+        }
+    }
+}
+
+#[allow(dead_code)]
+fn main() {
+    let data = [];
+
+    create_repo(&data);
+}
