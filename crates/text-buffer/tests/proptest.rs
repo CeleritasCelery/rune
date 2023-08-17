af00e63f3b115c0b8d077ad65487eba3c8637e5c
Troy Hinckley <t.macman@gmail.com>
2023-08-17 17:57:24 -0500

    Refactor proptests

--- crates/text-buffer/tests/proptest.rs
+++ crates/text-buffer/tests/proptest.rs
@@ -9,12 +9,34 @@ fn string_insert(text: &mut String, char_idx: usize, text_ins: &str) {
     text.insert_str(byte_idx, text_ins);
 }
 
-fn string_remove(text: &mut String, char_start: usize, char_end: usize) {
+fn string_remove(text: &mut String, mut char_start: usize, mut char_end: usize) {
+    if char_start > char_end {
+        std::mem::swap(&mut char_start, &mut char_end);
+    }
     let byte_start = to_byte_idx(text, char_start);
     let byte_end = to_byte_idx(text, char_end);
     let text_r = text.split_off(byte_end);
     text.truncate(byte_start);
     text.push_str(&text_r);
+}
+
+fn insert(buffer: &mut Buffer, string: &mut String, pos: usize, ins_text: &str) {
+    let len = buffer.len_chars();
+    buffer.set_cursor(pos % (len + 1));
+    buffer.insert(ins_text);
+    string_insert(string, pos % (len + 1), ins_text);
+
+    assert_eq!(buffer, &&**string);
+}
+
+fn delete(buffer: &mut Buffer, string: &mut String, beg: usize, end: usize) {
+    let len = buffer.len_chars();
+    let beg = beg % (len + 1);
+    let end = end % (len + 1);
+    buffer.delete_range(beg, end);
+    string_remove(string, beg, end);
+
+    assert_eq!(buffer, &&**string);
 }
 
 proptest! {
@@ -28,31 +50,25 @@ proptest! {
 
     #[test]
     fn pt_insert(char_idx in any::<usize>(), ref mut text in "\\PC*", ref ins_text in "\\PC*") {
-        let mut buffer = Buffer::from(&**text);
-
-        let len = buffer.len_chars();
-        buffer.set_cursor(char_idx % (len + 1));
-        buffer.insert(ins_text);
-        string_insert(text, char_idx % (len + 1), ins_text);
-
-        assert_eq!(buffer, text);
+        let buffer = &mut Buffer::from(&**text);
+        insert(buffer, text, char_idx, ins_text);
     }
 
     #[test]
     fn pt_delete(beg in any::<usize>(), end in any::<usize>(), ref mut text in "\\PC*") {
-        let mut buffer = Buffer::from(&**text);
+        let buffer = &mut Buffer::from(&**text);
+        delete(buffer, text, beg, end);
+    }
 
-        let len = buffer.len_chars();
-        let beg = beg % (len + 1);
-        let end = end % (len + 1);
-        buffer.delete_range(beg, end);
-
-        if beg > end {
-            string_remove(text, end, beg);
+    #[test]
+    fn pt_combo(beg in any::<usize>(), end in any::<usize>(), char_idx in any::<usize>(), ref mut text in "\\PC*", ref ins_text in "\\PC*", ins_first in any::<bool>()) {
+        let buffer = &mut Buffer::from(&**text);
+        if ins_first {
+            insert(buffer, text, char_idx, ins_text);
+            delete(buffer, text, beg, end);
         } else {
-            string_remove(text, beg, end);
+            delete(buffer, text, beg, end);
+            insert(buffer, text, char_idx, ins_text);
         }
-
-        assert_eq!(buffer, text);
     }
 }
