3dc854c63e83c69436996106750e94222ce54bbb
Troy Hinckley <t.macman@gmail.com>
2023-09-22 16:38:24 -0500

    Add resize and move benchmarks

--- crates/text-buffer/benches/benches.rs
+++ crates/text-buffer/benches/benches.rs
@@ -1,5 +1,7 @@
 use crdt_testdata::{TestData, TestPatch};
-use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use criterion::{
+    black_box, criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion, Throughput,
+};
 use text_buffer::Buffer;
 
 fn get_test_data(name: &str) -> TestData {
@@ -45,5 +47,67 @@ fn real_world(c: &mut Criterion) {
     }
 }
 
-criterion_group!(benches, real_world);
+// function to create a random string of length `len`
+fn random_string(len: usize) -> String {
+    use rand::distributions::Alphanumeric;
+    use rand::{thread_rng, Rng};
+    thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(len)
+        .map(Into::<char>::into)
+        .collect()
+}
+
+fn resize(c: &mut Criterion) {
+    let mut group = c.benchmark_group("resize");
+
+    for (size, sample) in &[(10, 100), (15, 100), (20, 50), (25, 10), (30, 10)] {
+        group.sample_size(*sample);
+        let size = &usize::pow(2, *size);
+        let id = BenchmarkId::from_parameter(size);
+        // because we create the buffer from a String, the gap size will be 0,
+        // meaning any insert will resize it
+        group.bench_function(id, |b| {
+            b.iter_batched(
+                || {
+                    let string = random_string(*size);
+                    Buffer::from(string)
+                },
+                |mut buffer| {
+                    buffer.insert_char('a');
+                },
+                BatchSize::SmallInput,
+            );
+        });
+    }
+    group.finish();
+}
+
+fn move_cursor(c: &mut Criterion) {
+    let mut group = c.benchmark_group("move_cursor");
+
+    for (size, sample) in &[(10, 100), (15, 100), (20, 50), (25, 10), (30, 10)] {
+        group.sample_size(*sample);
+        let size = &usize::pow(2, *size);
+        let id = BenchmarkId::from_parameter(size);
+        group.bench_function(id, |b| {
+            b.iter_batched(
+                || {
+                    let string = random_string(*size);
+                    // create from a reference
+                    Buffer::from(&*string)
+                },
+                |mut buffer| {
+                    let len = buffer.len_chars();
+                    buffer.set_cursor(len);
+                    buffer.insert_char('a');
+                },
+                BatchSize::SmallInput,
+            );
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, real_world, resize, move_cursor);
 criterion_main!(benches);
