871256c8d46fbb1bd1d71bca6f8fb45d3cdaa9d9
Troy Hinckley <t.macman@gmail.com>
2023-11-14 10:41:06 -0600

    Define max-char and characterp

--- src/character.rs
+++ src/character.rs
@@ -1,4 +1,4 @@
-use crate::core::object::Gc;
+use crate::core::object::{Gc, GcObj, Object};
 use anyhow::Result;
 use fn_macros::defun;
 
@@ -6,4 +6,24 @@ use fn_macros::defun;
 fn unibyte_string(bytes: &[Gc<i64>]) -> Result<Vec<u8>> {
     let unibyte: Result<Vec<u8>, _> = bytes.iter().map(|x| u8::try_from(x.untag())).collect();
     Ok(unibyte?)
+}
+
+#[defun]
+fn max_char(unicode: Option<()>) -> u32 {
+    if unicode.is_some() {
+        std::char::MAX as u32
+    } else {
+        0x3F_FFFF
+    }
+}
+
+#[defun]
+fn characterp(obj: GcObj) -> bool {
+    match obj.untag() {
+        Object::Int(c) => match u32::try_from(c) {
+            Ok(c) => char::from_u32(c).is_some(),
+            Err(_) => false,
+        },
+        _ => false,
+    }
 }
