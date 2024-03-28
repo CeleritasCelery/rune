a72fdfc6874f2c42b564ab1f58e04b7c79cb19b7
dwuggh <dwuggh@gmail.com>
2024-03-29 00:56:00 +0800

    impl some function

--- src/textprops.rs
+++ src/textprops.rs
@@ -1,11 +1,17 @@
 use crate::{
     core::{
-        cons::Cons, env::Env, gc::{Context, ObjectMap, Rt}, object::{Gc, ListType, Object, ObjectType, WithLifetime, NIL, TRUE}
+        cons::Cons,
+        env::Env,
+        gc::{Context, Rt},
+        object::{Gc, ListType, Object, ObjectType, WithLifetime, NIL, TRUE},
     },
-    fns::{eq, plist_get}, intervals::IntervalTree,
+    editfns::point_max,
+    fns::{eq, plist_get},
+    intervals::{IntervalTree, TextRange},
 };
+use anyhow::{bail, Result};
 use rune_macros::defun;
-use anyhow::Result;
+use rune_core::macros::list;
 
 #[allow(dead_code)]
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
@@ -15,17 +21,17 @@ pub enum PropertySetType {
     Append,
 }
 
-// Add the properties of PLIST to the interval I, or set
-//    the value of I's property to the value of the property on PLIST
-//    if they are different.
-
-//    OBJECT should be the string or buffer the interval is in.
-
-//    If DESTRUCTIVE, the function is allowed to reuse list values in the
-//    properties.
-
-//    Return true if this changes I (i.e., if any members of PLIST
-//    are actually added to I's plist)
+/// Add the properties of PLIST to the interval I, or set
+/// the value of I's property to the value of the property on PLIST
+/// if they are different.
+///
+/// OBJECT should be the string or buffer the interval is in.
+///
+/// If DESTRUCTIVE, the function is allowed to reuse list values in the
+/// properties.
+///
+/// Return true if this changes I (i.e., if any members of PLIST
+/// are actually added to I's plist)
 pub fn add_properties<'ob>(
     plist: Object<'ob>,
     mut obj_i: Object<'ob>,
@@ -69,20 +75,38 @@ pub fn add_properties<'ob>(
 
 pub fn plist_get_str<'ob>(plist: Object<'ob>, prop: &str) -> Result<Object<'ob>> {
     let Ok(plist) = Gc::<ListType>::try_from(plist) else { return Ok(NIL) };
-    
+
     // TODO: this function should never fail. Need to implement safe iterator
     let mut iter = plist.elements();
     while let Some(cur_prop) = iter.next() {
         let Some(value) = iter.next() else { return Ok(NIL) };
         if let ObjectType::Symbol(sym) = cur_prop?.untag() {
             if sym.to_string() == prop {
-                return Ok(value?)
+                return Ok(value?);
             }
         }
     }
     Ok(NIL)
 }
 
+/// Get the property interval tree for OBJECT. OBJECT is a buffer or string.
+/// If OBJECT is NIL, return current buffer's interval tree.
+fn prop_tree_for_object<'a, 'ob, 'env>(
+    object: Object<'ob>,
+    env: &'a mut Rt<Env<'env>>,
+) -> Result<&'a mut IntervalTree<'env>> {
+    let tree = match object.untag() {
+        ObjectType::Buffer(buf) => env.get_buffer_textprops(buf)?,
+        ObjectType::NIL => {
+            let Some(tree) = env.current_buffer_textprops() else { bail!("no current buffer") };
+            tree
+        }
+        _ => {
+            bail!("wrong type argument: not a buffer");
+        }
+    };
+    Ok(tree)
+}
 
 /// Return the list of properties of the character at POSITION in OBJECT.
 /// If the optional second argument OBJECT is a buffer (or nil, which means
@@ -98,16 +122,19 @@ pub fn plist_get_str<'ob>(plist: Object<'ob>, prop: &str) -> Result<Object<'ob>>
 /// If you want to display the text properties at point in a human-readable
 /// form, use the `describe-text-properties' command.
 #[defun]
-pub fn text_properties_at<'ob>(position: usize, object: Object<'ob>, env: &Rt<Env>) -> Object<'ob> {
-    if let Some(tree) = env.buffer_textprops.get(&object) {
-        if let Some(prop) = tree.find(position) {
-            // TODO lifetimes don't match; don't know why yet
-            unsafe {
-                return prop.val.with_lifetime();
-            }
+pub fn text_properties_at<'ob>(
+    position: usize,
+    object: Object<'ob>,
+    env: &mut Rt<Env>,
+) -> Result<Object<'ob>> {
+    let tree = prop_tree_for_object(object, env)?;
+    if let Some(prop) = tree.find(position) {
+        // TODO lifetimes don't match; don't know why yet
+        unsafe {
+            return Ok(prop.val.with_lifetime());
         }
     }
-    NIL
+    Ok(NIL)
 }
 /// Return the value of POSITION's property PROP, in OBJECT.
 /// OBJECT should be a buffer or a string; if omitted or nil, it defaults
@@ -118,8 +145,13 @@ pub fn text_properties_at<'ob>(position: usize, object: Object<'ob>, env: &Rt<En
 /// narrowed and POSITION is at the end of the narrowed buffer, the result
 /// may be non-nil.
 #[defun]
-pub fn get_text_property<'ob>(position: usize, prop: Object<'ob>, object: Object<'ob>, env: &Rt<Env>) -> Result<Object<'ob>> {
-    let props = text_properties_at(position, object, env);
+pub fn get_text_property<'ob>(
+    position: usize,
+    prop: Object<'ob>,
+    object: Object<'ob>,
+    env: &mut Rt<Env>,
+) -> Result<Object<'ob>> {
+    let props = text_properties_at(position, object, env)?;
     // TODO see lookup_char_property, should also lookup
     // 1. category
     // 2. char_property_alias_alist
@@ -127,31 +159,64 @@ pub fn get_text_property<'ob>(position: usize, prop: Object<'ob>, object: Object
     plist_get(props, prop)
 }
 
-
+/// Set one property of the text from START to END.
+/// The third and fourth arguments PROPERTY and VALUE
+/// specify the property to add.
+/// If the optional fifth argument OBJECT is a buffer (or nil, which means
+/// the current buffer), START and END are buffer positions (integers or
+/// markers).  If OBJECT is a string, START and END are 0-based indices into it.
 #[defun]
-pub fn put_text_property<'ob>(start: usize, end: usize, property: Object<'ob>, value: Object<'ob>, object: Object<'ob>, env: &mut Rt<Env>) {
-    // let props = env.buffer_textprops(object);
-    // env.buffer_textprops.entry(object).or_insert(IntervalTree::new());
-    
+pub fn put_text_property<'ob>(
+    start: usize,
+    end: usize,
+    property: Object<'ob>,
+    value: Object<'ob>,
+    object: Object<'ob>,
+    cx: &'ob Context,
+    env: &mut Rt<Env>,
+) -> Result<()> {
+    let tree = prop_tree_for_object(object, env)?;
+    let prop: Object<'ob> = list!(property, value; cx);
+    tree.insert(TextRange::new(start, end), prop, cx);
+    Ok(())
 }
 
 #[defun]
-pub fn next_property_change<'ob>(position: usize, object: Object<'ob>, limit: Object<'ob>, env: &mut Rt<Env>) -> Result<usize> {
-    let env = &mut **env;
-    let tree = if object.is_nil() {
-        todo!()
-    } else {
-        // env.buffer_textprops(object).ok_or(anyhow::Error::msg("no property tree for buffer"))?
-    };
+pub fn next_property_change<'ob>(
+    position: usize,
+    object: Object<'ob>,
+    limit: Object<'ob>,
+    cx: &'ob Context,
+    env: &mut Rt<Env>,
+) -> Result<Object<'ob>> {
+    let tree = prop_tree_for_object(object, env)?;
+    // let point_max = point_max(env)?;
     // let node = tree.find(position);
+    // let nodes = tree.find_intersects(TextRange::new(position, point_max));
 
-    if eq(limit, TRUE) {
-        todo!()
-
-    }
+    // If LIMIT is t, return start of next interval--don't
+    // bother checking further intervals.
+    // if eq(limit, TRUE) {
+    //     let pos = if let Some(node) = node {
+    //         if let Some(n) = node.next() {
+    //             n.key.end
+    //         } else {
+    //             return Ok(NIL);
+    //         }
+    //     } else {
+    //         // we don't have a text property interval around position
+    //         if let Some(n) =
+    //             tree.find_intersects(TextRange::new(position, point_max(env)?)).first().copied()
+    //         {
+    //             n.key.start
+    //         } else {
+    //             return Ok(NIL);
+    //         }
+    //     };
+    //     return Ok(cx.add(pos));
+    // };
 
     todo!()
-    
 }
 
 #[cfg(test)]
