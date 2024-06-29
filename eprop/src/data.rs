d903730cf79a12f7cdb5a639ee3add31c1676d24
ki11errabbit <ki11errabbit@posteo.net>
2024-06-29 15:26:46 -0700

    got simple parts put in for tester

---
+++ eprop/src/data.rs
@@ -0,0 +1,392 @@
+use arbitrary::Arbitrary;
+use syn::ItemFn;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum ObjectType {
+    String,
+    Float,
+    Cons,
+    Symbol,
+    Integer,
+    Boolean,
+    Unknown,
+    
+}
+
+impl Into<ArbitraryObjectType> for ObjectType {
+    fn into(self) -> ArbitraryObjectType {
+        let mut list = rand::random::<[u8; 32]>();
+        let mut unstructured = arbitrary::Unstructured::new(&mut list);
+        match self {
+            ObjectType::String => ArbitraryObjectType::String(String::arbitrary(&mut unstructured).unwrap()),
+            ObjectType::Float => ArbitraryObjectType::Float(f64::arbitrary(&mut unstructured).unwrap()),
+            ObjectType::Cons => ArbitraryObjectType::Cons(Box::new(ArbitraryObjectType::arbitrary(&mut unstructured).unwrap()), Box::new(ArbitraryObjectType::arbitrary(&mut unstructured).unwrap())),
+            ObjectType::Symbol => ArbitraryObjectType::Symbol(String::arbitrary(&mut unstructured).unwrap()),
+            ObjectType::Integer => ArbitraryObjectType::Integer(i64::arbitrary(&mut unstructured).unwrap()),
+            ObjectType::Boolean => ArbitraryObjectType::Boolean(bool::arbitrary(&mut unstructured).unwrap()),
+            ObjectType::Unknown => ArbitraryObjectType::Unknown(Box::new(ArbitraryObjectType::arbitrary(&mut unstructured).unwrap())),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Arbitrary)]
+pub enum ArbitraryObjectType {
+    String(String),
+    Float(f64),
+    Cons(Box<ArbitraryObjectType>, Box<ArbitraryObjectType>),
+    Symbol(String),
+    Integer(i64),
+    Boolean(bool),
+    Unknown(Box<ArbitraryObjectType>),
+}
+
+impl std::fmt::Display for ArbitraryObjectType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ArbitraryObjectType::String(s) => {
+                write!(f, "\"{}\"", s)
+            },
+            ArbitraryObjectType::Float(n) => {
+                write!(f, "{}", n)
+            },
+            ArbitraryObjectType::Cons(car, cdr) => {
+                write!(f, "({} . {})", car, cdr)
+            },
+            ArbitraryObjectType::Symbol(s) => {
+                write!(f, "{}", s)
+            },
+            ArbitraryObjectType::Integer(n) => {
+                write!(f, "{}", n)
+            },
+            ArbitraryObjectType::Boolean(b) => {
+                if *b {
+                    write!(f, "t")
+                } else {
+                    write!(f, "nil")
+                }
+            },
+            ArbitraryObjectType::Unknown(obj) => {
+                write!(f, "{}", obj)
+            },
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Type {
+    Object(Vec<ObjectType>),
+    Nil,
+}
+
+#[derive(Debug, Clone, PartialEq, Arbitrary)]
+pub enum ArbitraryType {
+    Object(ArbitraryObjectType),
+    Nil,
+}
+
+impl Into<ArbitraryType> for Type {
+    fn into(self) -> ArbitraryType {
+        match self {
+            Type::Object(obj) => {
+                let choice = rand::random::<usize>() % obj.len();
+                let obj = obj[choice];
+                ArbitraryType::Object(obj.into())
+            },
+            Type::Nil => ArbitraryType::Nil,
+        }
+    }
+}
+
+impl std::fmt::Display for ArbitraryType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ArbitraryType::Object(obj) => {
+                write!(f, "{}", obj)
+            },
+            ArbitraryType::Nil => {
+                write!(f, "nil")
+            },
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum ArgType {
+    Required(Type),
+    Optional(Type),
+}
+
+pub enum ArbitraryArgType {
+    Required(ArbitraryType),
+    Optional(ArbitraryType),
+}
+
+impl Into<ArbitraryArgType> for ArgType {
+    fn into(self) -> ArbitraryArgType {
+        match self {
+            ArgType::Required(ty) => {
+                ArbitraryArgType::Required(ty.into())
+            },
+            ArgType::Optional(ty) => {
+                ArbitraryArgType::Optional(ty.into())
+            },
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Function {
+    name: String,
+    args: Vec<ArgType>,
+    ret: Type,
+    fallible: bool,
+}
+
+impl Function {
+
+    fn process_arg(ty: &syn::Type) -> Option<Type> {
+
+        match ty {
+            syn::Type::Array(_) => {
+                todo!()
+            },
+            syn::Type::BareFn(_) => {
+                todo!()
+            },
+            syn::Type::Group(syn::TypeGroup{ elem, ..}) => {
+
+                Function::process_arg(elem)
+            },
+            syn::Type::ImplTrait(_) => {
+                todo!()
+            },
+            syn::Type::Infer(_) => {
+                todo!()
+            },
+            syn::Type::Macro(_) => {
+                todo!()
+            },
+            syn::Type::Never(_) => {
+                todo!()
+            },
+            syn::Type::Paren(_) => {
+                todo!()
+            },
+            syn::Type::Path(syn::TypePath { path, .. }) => {
+                let segments = &path.segments;
+                let last = segments.last().unwrap().ident.to_string();
+                
+                match last.as_str() {
+                    "StringOrSymbol" => {
+                        return Some(Type::Object(vec![ObjectType::String, ObjectType::Symbol]));
+                    },
+                    "Number" => {
+                        return Some(Type::Object(vec![ObjectType::Integer, ObjectType::Float]));
+                    },
+                    "Object" => {
+                        return Some(Type::Object(vec![ObjectType::Unknown]));
+                    },
+                    "usize" => {
+                        return Some(Type::Object(vec![ObjectType::Integer]));
+                    },
+                    "str" => {
+                        return Some(Type::Object(vec![ObjectType::String]));
+                    },
+                    "bool" => {
+                        return Some(Type::Object(vec![ObjectType::Boolean]));
+                    },
+                    _ => {
+                        panic!("Unknown type: {}", last);
+                    }
+                }
+            },
+            syn::Type::Ptr(_) => {
+                todo!()
+            },
+            syn::Type::Reference(syn::TypeReference { elem, .. }) => {
+                Function::process_arg(elem)
+            },
+            syn::Type::Slice(_) => {
+                todo!()
+            },
+            syn::Type::TraitObject(_) => {
+                todo!()
+            },
+            syn::Type::Tuple(_) => {
+                todo!()
+            },
+            syn::Type::Verbatim(stream) => {
+                for token in stream.clone().into_iter() {
+                    match token {
+                        proc_macro2::TokenTree::Ident(ident) => {
+                            let source_text = ident.span().source_text().unwrap();
+                            match source_text.as_str() {
+                                "StringOrSymbol" => {
+                                    return Some(Type::Object(vec![ObjectType::String, ObjectType::Symbol]));
+                                },
+                                "Number" => {
+                                    return Some(Type::Object(vec![ObjectType::Integer, ObjectType::Float]));
+                                },
+                                "Object" => {
+                                    return Some(Type::Object(vec![ObjectType::Unknown]));
+                                },
+                                "usize" => {
+                                    return Some(Type::Object(vec![ObjectType::Integer]));
+                                },
+                                "str" => {
+                                    return Some(Type::Object(vec![ObjectType::String]));
+                                },
+                                _ => {
+                                    panic!("Unknown type: {}", source_text);
+                                }
+                            }
+                        },
+                        proc_macro2::TokenTree::Punct(punct) if punct.as_char() == ')' => {
+                            return Some(Type::Object(vec![ObjectType::Boolean]));
+                        }, 
+                        _ => continue,
+                    }
+                }
+                None
+            },
+            _ => {
+                todo!()
+            },
+        }
+    }
+}
+
+impl From<&ItemFn> for Function {
+    fn from(item: &ItemFn) -> Self {
+        println!("{}", item.sig.ident.to_string());
+        let name = item.sig.ident.to_string().chars().map(|c| match c {
+            '_' => '-',
+            c => c,
+        }).collect();
+
+        let args = item.sig.inputs.iter().map(|arg| {
+            let ty = match arg {
+                syn::FnArg::Receiver(syn::Receiver { ty, .. }) => {
+                    ty
+                },
+                syn::FnArg::Typed(syn::PatType { ty, .. }) => {
+                    ty
+                },
+            };
+
+            match ty.as_ref() {
+                syn::Type::Group(syn::TypeGroup{ group_token, elem }) => {
+                    let syn::token::Group { span } = group_token;
+
+                    let source_text = span.source_text().unwrap();
+                    let optional = match source_text.as_str() {
+                        "Option" => true,
+                        _ => false,
+                    };
+
+                    let ty = Function::process_arg(elem).unwrap();
+
+                    match ty {
+                        Type::Object(ref obj) => {
+                            if obj.contains(&ObjectType::Boolean) {
+                                ArgType::Required(ty)
+                            } else {
+                                if optional {
+                                    ArgType::Optional(ty)
+                                } else {
+                                    ArgType::Required(ty)
+                                }
+                            }
+                        },
+                        _ => {
+                            if optional {
+                                ArgType::Optional(ty)
+                            } else {
+                                ArgType::Required(ty)
+                            }
+                        }
+                    }
+                },
+                x => {
+                    let ty = Function::process_arg(x).unwrap();
+
+                    ArgType::Required(ty)
+                },
+            }
+
+        }).collect();
+
+        let (ret, fallible) = match &item.sig.output {
+            syn::ReturnType::Default => (Type::Nil, false),
+            syn::ReturnType::Type(_, ty) => {
+                match ty.as_ref() {
+                    syn::Type::Group(syn::TypeGroup{ group_token, elem }) => {
+                        let syn::token::Group { span } = group_token;
+
+                        let source_text = span.source_text().unwrap();
+                        let fallible = match source_text.as_str() {
+                            "Option" => true,
+                            "Result" => true,
+                            _ => false,
+                        };
+
+                        let ty = Function::process_arg(elem).unwrap();
+                        (ty, fallible)
+                    },
+                    x => {
+                        let ty = Function::process_arg(x).unwrap();
+
+                        (ty, false)
+                    },
+                }
+            },
+        };
+
+        
+       Function {
+            name,
+            args,
+            ret,
+            fallible,
+       }
+
+    }
+}
+
+pub struct ArbitraryFunction {
+    name: String,
+    args: Vec<ArbitraryArgType>,
+    ret: ArbitraryType,
+    fallible: bool,
+}
+
+impl Into<ArbitraryFunction> for Function {
+    fn into(self) -> ArbitraryFunction {
+        ArbitraryFunction {
+            name: self.name,
+            args: self.args.into_iter().map(|arg| arg.into()).collect(),
+            ret: self.ret.into(),
+            fallible: self.fallible,
+        }
+    }
+}
+
+impl std::fmt::Display for ArbitraryFunction {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({} ", self.name)?;
+        let optionals =rand::random::<bool>();
+        for arg in &self.args {
+            match arg {
+                ArbitraryArgType::Required(ty) => {
+                    write!(f, "{} ", ty)?;
+                },
+                ArbitraryArgType::Optional(ty) => {
+                    if optionals {
+                        write!(f, "{} ", ty)?;
+                    }
+                },
+            }
+        }
+        write!(f, ")")
+    }
+}
---
+++ eprop/src/generate.rs
@@ -0,0 +1,49 @@
+use crate::data::{self, ArbitraryFunction};
+
+
+
+
+pub fn generate_sigs(string: &str) -> Vec<data::Function> {
+    let file = syn::parse_file(string).unwrap();
+
+    let functions = file.items.iter().filter_map(|item| {
+        if let syn::Item::Fn(func) = item {
+            let mut is_defun = false;
+            for attr in &func.attrs {
+                if let syn::Meta::Path(path) = &attr.meta {
+                    if let Some(ident) = path.get_ident() {
+                        if ident == "defun" {
+                            is_defun = true;
+                        }
+                    }
+                }
+            }
+            if !is_defun {
+                return None;
+            }
+            Some(data::Function::from(func))
+        } else {
+            None
+        }
+    }).collect();
+
+    functions
+}
+
+
+pub fn generate_tests(function: &data::Function, test_count: usize) -> Vec<data::ArbitraryFunction> {
+    let mut tests: Vec<data::ArbitraryFunction> = Vec::new();
+    for _ in 0..test_count {
+        let function = function.clone();
+        tests.push(function.into());
+    }
+    tests
+}
+
+pub fn generate_test_string(tests: Vec<data::ArbitraryFunction>) -> String {
+    let mut test_string = String::new();
+    for test in tests {
+        test_string.push_str(&test.to_string());
+    }
+    test_string
+}
