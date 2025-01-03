a3f7da0b262170da3a3bf8b07fab94da83dd139e
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2025-01-03 22:46:56 +0100

    feat(casefiddle): handle unicode in upcase

--- src/casefiddle.rs
+++ src/casefiddle.rs
@@ -8,6 +8,27 @@ fn capitalize(s: &str) -> String {
 
 #[defun]
 fn upcase(s: &str) -> String {
-    // TODO: use unicode
-    s.to_uppercase()
+    let mut result = String::new();
+    for c in s.chars() {
+        match c {
+            '\u{10D40}'..='\u{10D8F}' => result.push(c), // ignores range not supported by Emacs yet
+            _ => {
+                for uc in c.to_uppercase() {
+                    result.push(uc);
+                }
+            }
+        }
+    }
+    result.replace('\\', "\\\\").replace('"', "\\\"")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ignore_characters_in_garay_unicode_block() {
+        assert_eq!("𐵰", upcase("𐵰"));
+        assert_eq!("𐵪", upcase("𐵪"));
+    }
 }
