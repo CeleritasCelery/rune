a35a79998f18b9afac955ce296abd27d95a534cb
Hesam Pakdaman <hesam.pakdaman@gmail.com>
2025-01-04 10:52:50 +0100

    feat(casefiddle): handle unicode in upcase

--- src/casefiddle.rs
+++ src/casefiddle.rs
@@ -8,6 +8,42 @@ fn capitalize(s: &str) -> String {
 
 #[defun]
 fn upcase(s: &str) -> String {
-    // TODO: use unicode
-    s.to_uppercase()
+    s.chars()
+        .flat_map(|c| c.to_uppercase())
+        .collect::<String>()
+        // re-escape string
+        .replace('\\', "\\\\")
+        .replace('"', "\\\"")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_upcase() {
+        // Basic escape characters
+        assert_eq!("\n", upcase("\n"));
+        assert_eq!("\t", upcase("\t"));
+        assert_eq!("\r", upcase("\r"));
+        assert_eq!(r#"\""#, upcase("\""));
+        assert_eq!(r#"\\"#, upcase("\\"));
+
+        // Control characters
+        assert_eq!("\u{0}", upcase("\u{0}"));
+        assert_eq!("\u{1B}", upcase("\u{1B}"));
+        assert_eq!("\u{7F}", upcase("\u{7F}"));
+
+        // Already escaped
+        assert_eq!(r#"\\N"#, upcase("\\n"));
+
+        // Non-ASCII characters
+        assert_eq!("ΑΒΓ", upcase("αβγ"));
+        assert_eq!("ÅÄÖ", upcase("åäö"));
+
+        // Mixed content
+        assert_eq!("HELLO\nWORLD", upcase("hello\nworld"));
+        assert_eq!("FOO\tBAR", upcase("foo\tbar"));
+        assert_eq!(r#"PATH\\TO\\FILE\"NAME\""#, upcase("path\\to\\file\"name\""));
+    }
 }
