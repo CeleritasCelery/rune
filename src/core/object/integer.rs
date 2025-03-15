5506b50a92ecceae1bfbd0011073914e2a582219
Nikhil K <nikhilkalige@gmail.com>
2025-03-14 22:32:29 -0700

    Add From conversions for FlexInt

--- src/core/object/integer.rs
+++ src/core/object/integer.rs
@@ -20,6 +20,9 @@ pub (crate) enum FlexInt {
 
 derive_GcMoveable!(LispInteger);
 
+impl FlexInt {
+}
+
 impl Trace for FlexInt {
     fn trace(&self, _: &mut GcState) {}
 }
@@ -27,5 +30,42 @@ impl Trace for FlexInt {
 impl<'new> CloneIn<'new, &'new FlexInt> for FlexInt {
     fn clone_in<const C: bool>(&self, bk: &'new Block<C>) -> super::Gc<&'new Self> {
         todo!()
+        BigInt::from(1 as u128);
+    }
+}
+
+macro_rules! impl_from_small_integer {
+    ($($num_type:ty),+) => {
+        $(
+            impl From<$num_type> for FlexInt {
+                fn from(value: $num_type) -> Self {
+                    FlexInt::Fixed(value as i64)
+                }
+            }
+        )+
+    }
+}
+
+macro_rules! impl_from_large_integer {
+    ($($num_type:ty),+) => {
+        $(
+            impl From<$num_type> for FlexInt {
+                fn from(value: $num_type) -> Self {
+                    FlexInt::Big(BigInt::from(value))
+                }
+            }
+        )+
+    }
+}
+
+// From implementations for types that fit into i64
+impl_from_small_integer!(u8, u16, u32, i8, i16, i32, i64);
+
+// From implementations for types that overflow i64
+impl_from_large_integer!(u64, u128, usize, i128, isize);
+
+impl From<BigInt> for FlexInt {
+    fn from(value: BigInt) -> Self {
+        FlexInt::Big(value)
     }
 }
