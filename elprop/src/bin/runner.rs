7687f0d1f74d14e89959cdfc03e203f800dd047a
Nikhil Kalige <nikhilkalige@users.noreply.github.com>
2025-04-11 14:33:21 -0700

    Big int support (#114)
    * Start adding Bigint support
    
    * Add From conversions for FlexInt
    
    * More tags
    
    * Implement clone_in
    
    * More progress
    
    * Count number of iterations
    
    * More progress
    
    * Fixed int overflow issues
    
    * Round function
    
    * Float functions
    
    * More float functions
    
    * Cleanup unnecessary changes

--- elprop/src/bin/runner.rs
+++ elprop/src/bin/runner.rs
@@ -44,8 +44,11 @@ fn main() {
 
     let outputs = RefCell::new(Vec::new());
     let regex = regex::Regex::new("^\\((wrong-[a-zA-Z0-9-]*|[a-zA-Z0-9-]*error) ").unwrap();
+
     for func in config.functions {
         let name = func.name.clone();
+        let func_iter_count = RefCell::new(0);
+
         let result = runner.run(&func.strategy(), |input| {
             if *rune_panicked.borrow() {
                 return Err(TestCaseError::Reject("Rune panicked".into()));
@@ -82,6 +85,7 @@ fn main() {
             println!(";; done");
 
             *master_count.borrow_mut() += 1;
+            *func_iter_count.borrow_mut() += 1;
 
             match (emacs_output, rune_output) {
                 (Ok(e), Ok(r)) if e == r => Ok(()),
@@ -102,7 +106,7 @@ fn main() {
             Err(TestError::Abort(reason)) => Status::Abort(reason.to_string()),
             Ok(()) => Status::Pass,
         };
-        let output = Output { function: name, status };
+        let output = Output { function: name, count: *func_iter_count.borrow(), status };
         outputs.borrow_mut().push(output);
     }
 
--- src/floatfns.rs
+++ src/floatfns.rs
@@ -1,4 +1,6 @@
 //! Operations on floats.
+use std::ops::{AddAssign, BitAnd, Div, Rem, SubAssign};
+
 use crate::{
     arith::NumberValue,
     core::{
@@ -7,6 +9,11 @@ use crate::{
         object::{Number, NumberType, Object},
     },
 };
+use anyhow::Result;
+use anyhow::anyhow;
+use num_bigint::BigInt;
+use num_integer::Integer;
+use num_traits::{FromPrimitive, Signed, ToPrimitive, Zero};
 
 use rune_macros::defun;
 
@@ -15,59 +22,212 @@ fn coerce(arg: Number) -> f64 {
     match arg.untag() {
         NumberType::Int(i) => i as f64,
         NumberType::Float(f) => **f,
+        NumberType::Big(b) => b.to_f64().unwrap(), // TODO: Handle big integers
     }
 }
 
-#[defun]
-fn floor(arg: Number, divisor: Option<Number>) -> i64 {
-    let num = match divisor {
-        Some(div) => arg.val() / div.val(),
-        None => arg.val(),
+/* Return the integer exponent E such that D * FLT_RADIX**E (i.e.,
+scalbn (D, E)) is an integer that has precision equal to D and is
+representable as a double.
+
+Return DBL_MANT_DIG - DBL_MIN_EXP (the maximum possible valid
+scale) if D is zero or tiny.  Return one greater than that if
+D is infinite, and two greater than that if D is a NaN.  */
+fn double_integer_scale(d: f64) -> i64 {
+    let exponent = frexp_f(d).1 - 1;
+
+    if f64::MIN_EXP as i64 - 1 <= exponent && exponent < i64::MAX {
+        f64::MANTISSA_DIGITS as i64 - 1 - exponent
+    } else {
+        let x = f64::MANTISSA_DIGITS as i64 - f64::MIN_EXP as i64;
+        let c = if exponent == i64::MAX { 1 } else { 0 };
+        if d.is_nan() { x + 2 } else { x + c }
+    }
+}
+
+fn rescale_for_division(n: NumberValue, nscale: i64, dscale: i64) -> Result<BigInt> {
+    let mut result = match n {
+        NumberValue::Int(i) => BigInt::from(i),
+        NumberValue::Float(f) => {
+            if (f64::MANTISSA_DIGITS as i64 - f64::MIN_EXP as i64) < nscale {
+                return Err(anyhow!("Overflow error"));
+            }
+            BigInt::from_f64(libm::scalbn(f, nscale as i32)).expect("Conversion error")
+        }
+        NumberValue::Big(b) => b,
     };
-    match num {
-        NumberValue::Int(i) => i,
-        NumberValue::Float(f) => f.floor() as i64,
+
+    if nscale < dscale {
+        let power = (dscale - nscale) * f64::RADIX.ilog2() as i64;
+        result *= BigInt::from(2).pow(power as u32);
+    }
+
+    Ok(result)
+}
+
+fn rounding_driver(
+    n: NumberValue,
+    d: Option<NumberValue>,
+    double_round: fn(f64) -> f64,
+    int_divide: fn(i64, i64) -> i64,
+    bigum_divide: fn(BigInt, BigInt) -> BigInt,
+) -> Result<NumberValue> {
+    let d = match d {
+        None => {
+            return Ok(match n {
+                NumberValue::Float(f) => NumberValue::Float(double_round(f)).coerce_integer(),
+                other => other,
+            });
+        }
+        Some(d) if d.is_zero() => {
+            return Err(anyhow!("(arith-error)"));
+        }
+        Some(d) => d,
+    };
+
+    match (n, d) {
+        (NumberValue::Int(n), NumberValue::Int(d)) => Ok(NumberValue::Int(int_divide(n, d))),
+        (n, d) => {
+            let dscale = match d {
+                NumberValue::Float(f) => double_integer_scale(f),
+                _ => 0,
+            };
+
+            let nscale = match n {
+                NumberValue::Float(f) => double_integer_scale(f),
+                _ => 0,
+            };
+
+            /* If the numerator is finite and the denominator infinite, the
+            quotient is zero and there is no need to try the impossible task
+            of rescaling the denominator.  */
+            if dscale == f64::MANTISSA_DIGITS as i64 - f64::MIN_EXP as i64 + 1 && nscale < dscale {
+                return Ok(NumberValue::Int(0));
+            }
+
+            let num = rescale_for_division(n, nscale, dscale)?;
+            let denom = rescale_for_division(d, dscale, nscale)?;
+            Ok(NumberValue::Big(bigum_divide(num, denom)).coerce_integer())
+        }
     }
 }
 
-#[defun]
-fn ceiling(arg: Number) -> i64 {
-    match arg.untag() {
-        NumberType::Int(i) => i,
-        NumberType::Float(f) => f.ceil() as i64,
+fn round2<T>(num: T, den: T) -> T
+where
+    T: Div<Output = T>
+        + Rem<Output = T>
+        + PartialOrd
+        + num_traits::Zero
+        + num_traits::Signed
+        + BitAnd<Output = T>
+        + AddAssign
+        + SubAssign
+        + Clone,
+{
+    // The C language's division operator gives us the remainder R
+    // corresponding to truncated division, but we want the remainder R1
+    // on the other side of 0 if R1 is closer to 0 than R is; because we
+    // want to round to even, we also want R1 if R and R1 are the same
+    // distance from 0 and if the truncated quotient is odd.
+    let mut q: T = num.clone() / den.clone();
+    let r: T = num.clone() % den.clone();
+    let neg_d = den < T::zero();
+    let neg_r = r < T::zero();
+    let abs_r: T = r.abs();
+    let abs_r1: T = den.abs() - abs_r.clone();
+
+    let increment = if (q.clone() & T::one()) == T::one() { T::zero() } else { T::one() };
+    if abs_r1 < abs_r + increment {
+        if neg_d == neg_r {
+            q += T::one();
+        } else {
+            q -= T::one();
+        }
     }
+    q
+}
+
+const LIMBS_LIMIT: usize = 2147483642;
+
+fn checked_pow(base: BigInt, exp: u32) -> Result<BigInt> {
+    // Check base size (number of limbs)
+    let nbase = base.bits() as usize; // Number of bits used by the base
+    let n = nbase * exp as usize; // Approximate number of limbs required for the result
+
+    // Check overflow condition
+    if n > LIMBS_LIMIT {
+        return Err(anyhow!("Overflow error".to_string()));
+    }
+    Ok(base.pow(exp))
 }
 
 #[defun]
-fn fceiling(arg: Number) -> f64 {
-    match arg.untag() {
-        NumberType::Int(i) => i as f64,
-        NumberType::Float(f) => f.ceil(),
-    }
+fn floor(num: Number, divisor: Option<Number>) -> Result<NumberValue> {
+    rounding_driver(
+        num.val(),
+        divisor.map(|d| d.val()),
+        |f| f.floor(),
+        |n, d| num_integer::Integer::div_floor(&n, &d),
+        |n, d| n.div_floor(&d),
+    )
 }
 
 #[defun]
-fn round(arg: Number) -> i64 {
-    match arg.untag() {
-        NumberType::Int(i) => i,
-        NumberType::Float(f) => f.round() as i64,
-    }
+fn ceiling(num: Number, divisor: Option<Number>) -> Result<NumberValue> {
+    rounding_driver(
+        num.val(),
+        divisor.map(|d| d.val()),
+        |f| f.ceil(),
+        |n, d| num_integer::Integer::div_ceil(&n, &d),
+        |n, d| n.div_ceil(&d),
+    )
 }
 
 #[defun]
-fn truncate(arg: Number) -> i64 {
-    match arg.untag() {
-        NumberType::Int(i) => i,
-        NumberType::Float(f) => f.trunc() as i64,
-    }
+fn round(num: Number, divisor: Option<Number>) -> Result<NumberValue> {
+    rounding_driver(
+        num.val(),
+        divisor.map(|d| d.val()), //
+        |f| f.round(),
+        round2,
+        round2,
+    )
 }
 
 #[defun]
-fn float<'ob>(arg: Number<'ob>, cx: &'ob Context) -> Number<'ob> {
-    match arg.untag() {
-        NumberType::Int(i) => cx.add_as(i as f64),
-        NumberType::Float(_) => arg,
-    }
+fn truncate(num: Number, divisor: Option<Number>) -> Result<NumberValue> {
+    rounding_driver(
+        num.val(),
+        divisor.map(|d| d.val()), //
+        |f| f,
+        |n, d| n.div(&d),
+        |n, d| n.div(&d),
+    )
+}
+
+#[defun]
+fn fceiling(num: f64) -> f64 {
+    num.ceil()
+}
+
+#[defun]
+fn ffloor(num: f64) -> f64 {
+    num.floor()
+}
+
+#[defun]
+fn fround(num: f64) -> f64 {
+    num.round()
+}
+
+#[defun]
+fn ftruncate(num: f64) -> f64 {
+    num.trunc()
+}
+
+#[defun]
+fn float(arg: Number) -> NumberValue {
+    NumberValue::Float(coerce(arg))
 }
 
 #[defun]
@@ -103,14 +263,14 @@ fn tan(arg: Number) -> f64 {
 #[defun]
 fn isnan(arg: Number) -> bool {
     match arg.untag() {
-        NumberType::Int(_) => false,
         NumberType::Float(f) => f.is_nan(),
+        _ => false,
     }
 }
 
 #[defun]
-fn copysign(x: Number, y: Number) -> f64 {
-    coerce(x).copysign(coerce(y))
+fn copysign(x: f64, y: f64) -> f64 {
+    x.copysign(y)
 }
 
 #[defun]
@@ -122,13 +282,34 @@ fn exp(arg: Number) -> f64 {
 fn expt(x: Number, y: Number) -> NumberValue {
     // If either is a float, we use the float version
     match (x.untag(), y.untag()) {
-        (NumberType::Int(x), NumberType::Int(y)) => NumberValue::Int(x.pow(y as u32)),
-        _ => {
-            let x = coerce(x);
-            let y = coerce(y);
-            NumberValue::Float(x.powf(y))
+        (NumberType::Float(_), _) | (_, NumberType::Float(_)) => {
+            return NumberValue::Float(coerce(x).powf(coerce(y)));
         }
+        (_, _) => {}
+    };
+
+    // Otherwise, we use the integer version
+    let bx = match x.untag() {
+        NumberType::Int(x) => BigInt::from(x),
+        NumberType::Big(b) => (*b).clone(),
+        NumberType::Float(x) => {
+            return NumberValue::Float(x.powf(coerce(y)));
+        }
+    };
+
+    let by = match y.untag() {
+        NumberType::Int(x) => u32::try_from(x).ok(),
+        NumberType::Big(b) => b.to_u32(),
+        NumberType::Float(y) => {
+            return NumberValue::Float(coerce(x).powf(**y));
+        }
+    };
+
+    if let Some(y) = by {
+        let result = checked_pow(bx, y).unwrap();
+        return NumberValue::Big(result).coerce_integer();
     }
+    NumberValue::Float(coerce(x).powf(coerce(y)))
 }
 
 #[defun]
@@ -150,12 +331,12 @@ fn abs(arg: Number) -> NumberValue {
     match arg.untag() {
         NumberType::Int(i) => NumberValue::Int(i.abs()),
         NumberType::Float(f) => NumberValue::Float(f.abs()),
+        NumberType::Big(b) => NumberValue::Big(b.abs()),
     }
 }
 
 #[defun]
 fn ldexp(s: Number, e: i64) -> f64 {
-    // TODO: overflow check -> bail!?
     coerce(s) * 2f64.powi(e as i32)
 }
 
@@ -166,22 +347,14 @@ fn logb(arg: Number) -> i64 {
     l2.floor() as i64
 }
 
-// Rust does not have frexp, so we have to implement it ourselves
-// Source: https://stackoverflow.com/questions/55690397/where-is-the-frexp-function-for-f32-in-rust
-fn frexp_f(s: f64) -> (f64, i64) {
-    if 0.0 == s {
-        (s, 0)
-    } else {
-        let lg2 = s.abs().log2();
-        let x = (lg2 - lg2.floor() - 1.0).exp2();
-        let exp = lg2.floor() + 1.0;
-        (s.signum() * x, exp as i64)
-    }
-}
-
 #[defun]
 fn frexp<'ob>(x: Number, cx: &'ob Context) -> Object<'ob> {
     let f = coerce(x);
     let (significand, exponent) = frexp_f(f);
     Cons::new(significand, exponent, cx).into()
+}
+
+fn frexp_f(f: f64) -> (f64, i64) {
+    let (x, exp) = libm::frexp(f);
+    (x, exp as i64)
 }
