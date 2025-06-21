e69a79aee433e404984b6882ee7e6b907dfeb11f
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-06-21 16:06:40 +0300

    Implement `find-file-name-handler`

--- src/lisp.rs
+++ src/lisp.rs
@@ -16,3 +16,19 @@ pub const CHAR_MODIFIER_MASK: u64 = {
         | CharBits::Ctl as u64
         | CharBits::Meta as u64
 };
+
+// Check quit-flag and quit if it is non-nil.  Typing C-g does not
+// directly cause a quit; it only sets Vquit_flag.  So the program
+// needs to call maybe_quit at times when it is safe to quit.  Every
+// loop that might run for a long time or might not exit ought to call
+// maybe_quit at least once, at a safe place.  Unless that is
+// impossible, of course.  But it is very desirable to avoid creating
+// loops where maybe_quit is impossible.
+//
+// If quit-flag is set to `kill-emacs' the SIGINT handler has received
+// a request to exit Emacs when it is safe to do.
+//
+// When not quitting, process any pending signals.
+pub fn maybe_quit() {
+    // TODO: Implement maybe_quit
+}
