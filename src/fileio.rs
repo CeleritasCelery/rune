7aeefef1fc3c611437946ecb433619b04e904b01
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-07-03 12:11:25 +0300

    Move `assert_elprop!` into `library`

--- src/fileio.rs
+++ src/fileio.rs
@@ -305,17 +305,11 @@ fn file_name_concat(directory: &str, rest_components: &[Object]) -> Result<Strin
 //   required by file-name-extension  & file-name-sans-extension library & file-relative-name functions (among others)
 #[cfg(test)]
 mod tests {
-    use std::{
-        process::{Child, Stdio},
-        thread::sleep,
-        time,
-    };
-
     use super::*;
     use crate::{
+        assert_elprop,
         core::{env::sym, gc::RootSet},
         data::set,
-        interpreter::eval,
         sym::INHIBIT_FILE_NAME_HANDLERS,
     };
     use rune_core::macros::{list, root};
@@ -395,103 +389,6 @@ mod tests {
         )
         .unwrap();
         find_file_name_handler("example", NIL, env, cx);
-    }
-
-    struct InferiorEmacs {
-        pub socket_name: String,
-        emacs_started: bool,
-        daemon: Option<Child>,
-    }
-
-    impl Default for InferiorEmacs {
-        fn default() -> Self {
-            Self { socket_name: String::from("rune-test"), emacs_started: false, daemon: None }
-        }
-    }
-
-    impl InferiorEmacs {
-        fn new() -> Self {
-            InferiorEmacs { socket_name: String::from("rune-test"), ..InferiorEmacs::default() }
-        }
-
-        fn start_daemon(&mut self) -> Result<()> {
-            if self.emacs_started {
-                return Ok(());
-            }
-
-            let fg_daemon_arg = format!("--fg-daemon={}", &self.socket_name);
-            self.daemon = Some(
-                std::process::Command::new("emacs")
-                    .args(["-Q", &fg_daemon_arg, "--eval", "(setq debug-on-error t)"])
-                    .stdout(Stdio::piped())
-                    .stderr(Stdio::piped())
-                    .spawn()?,
-            );
-
-            let mut max_retries = 20;
-            while !self.emacs_started {
-                max_retries -= 1;
-                if max_retries == 0 {
-                    bail!("Cannot start Emacs daemon");
-                }
-
-                sleep(time::Duration::from_millis(50));
-                self.emacs_started = std::process::Command::new("emacsclient")
-                    .args(["--socket-name", &self.socket_name])
-                    .args(["--eval", "(version)"])
-                    .output()
-                    .is_ok_and(|output| output.status.code().is_some_and(|c| c == 0));
-            }
-
-            Ok(())
-        }
-
-        fn eval(&mut self, lisp: &str) -> Result<String> {
-            if !self.emacs_started {
-                bail!("Emacs daemon wasn't started");
-            }
-            let emacs_client = std::process::Command::new("emacsclient")
-                .args(["--socket-name", &self.socket_name])
-                .args(["--eval", lisp])
-                .output()
-                .expect("Failed to run emacsclient");
-            if emacs_client.status.code().unwrap() != 0 {
-                let client_output = String::from_utf8_lossy(&emacs_client.stderr);
-                eprintln!("{}", &client_output);
-                let client_output = String::from_utf8_lossy(&emacs_client.stdout);
-                eprintln!("{}", &client_output);
-            }
-            let output = String::from_utf8_lossy(&emacs_client.stdout);
-            Ok(output.trim_end_matches("\n").to_string())
-        }
-    }
-
-    macro_rules! assert_elprop {
-        ($form:expr) => {{
-            let mut inf_emacs = InferiorEmacs::new();
-            inf_emacs.start_daemon().unwrap();
-            let emacs_result = inf_emacs.eval($form).unwrap();
-
-            let roots = &crate::core::gc::RootSet::default();
-            let cx = &mut Context::new(roots);
-            sym::init_symbols();
-            root!(env, new(Env), cx);
-            let rune_result = {
-                let obj = crate::reader::read($form, cx).unwrap().0;
-                root!(obj, cx);
-                match eval(obj, None, env, cx) {
-                    Ok(val) => format!("{val}"),
-                    Err(e) => format!("Error: {e}"),
-                }
-            };
-            if !(rune_result == emacs_result) {
-                panic!(
-                    r#"assertion `Rune vs Emacs` failed:
-  Rune: {rune_result}
- Emacs: {emacs_result}"#
-                )
-            }
-        };};
     }
 
     #[test]
---
+++ src/library/elprop.rs
@@ -0,0 +1,104 @@
+use anyhow::{Result, bail};
+use std::{
+    process::{Child, Stdio},
+    thread::sleep,
+    time,
+};
+
+pub(crate) struct InferiorEmacs {
+    pub socket_name: String,
+    emacs_started: bool,
+    daemon: Option<Child>,
+}
+
+impl Default for InferiorEmacs {
+    fn default() -> Self {
+        Self { socket_name: String::from("rune-test"), emacs_started: false, daemon: None }
+    }
+}
+
+impl InferiorEmacs {
+    pub fn new() -> Self {
+        InferiorEmacs { socket_name: String::from("rune-test"), ..InferiorEmacs::default() }
+    }
+
+    pub fn start_daemon(&mut self) -> Result<()> {
+        if self.emacs_started {
+            return Ok(());
+        }
+
+        let fg_daemon_arg = format!("--fg-daemon={}", &self.socket_name);
+        self.daemon = Some(
+            std::process::Command::new("emacs")
+                .args(["-Q", &fg_daemon_arg, "--eval", "(setq debug-on-error t)"])
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .spawn()?,
+        );
+
+        let mut max_retries = 20;
+        while !self.emacs_started {
+            max_retries -= 1;
+            if max_retries == 0 {
+                bail!("Cannot start Emacs daemon");
+            }
+
+            sleep(time::Duration::from_millis(50));
+            self.emacs_started = std::process::Command::new("emacsclient")
+                .args(["--socket-name", &self.socket_name])
+                .args(["--eval", "(version)"])
+                .output()
+                .is_ok_and(|output| output.status.code().is_some_and(|c| c == 0));
+        }
+
+        Ok(())
+    }
+
+    pub fn eval(&mut self, lisp: &str) -> Result<String> {
+        if !self.emacs_started {
+            bail!("Emacs daemon wasn't started");
+        }
+        let emacs_client = std::process::Command::new("emacsclient")
+            .args(["--socket-name", &self.socket_name])
+            .args(["--eval", lisp])
+            .output()
+            .expect("Failed to run emacsclient");
+        if emacs_client.status.code().unwrap() != 0 {
+            let client_output = String::from_utf8_lossy(&emacs_client.stderr);
+            eprintln!("{}", &client_output);
+            let client_output = String::from_utf8_lossy(&emacs_client.stdout);
+            eprintln!("{}", &client_output);
+        }
+        let output = String::from_utf8_lossy(&emacs_client.stdout);
+        Ok(output.trim_end_matches("\n").to_string())
+    }
+}
+
+#[macro_export]
+macro_rules! assert_elprop {
+    ($form:expr) => {{
+        let mut inf_emacs = crate::library::elprop::InferiorEmacs::new();
+        inf_emacs.start_daemon().unwrap();
+        let emacs_result = inf_emacs.eval($form).unwrap();
+
+        let roots = &crate::core::gc::RootSet::default();
+        let cx = &mut Context::new(roots);
+        sym::init_symbols();
+        root!(env, new(Env), cx);
+        let rune_result = {
+            let obj = crate::reader::read($form, cx).unwrap().0;
+            root!(obj, cx);
+            match crate::interpreter::eval(obj, None, env, cx) {
+                Ok(val) => format!("{val}"),
+                Err(e) => format!("Error: {e}"),
+            }
+        };
+        if !(rune_result == emacs_result) {
+            panic!(
+                r#"assertion `Rune vs Emacs` failed:
+  Rune: {rune_result}
+ Emacs: {emacs_result}"#
+            )
+        }
+    };};
+}
