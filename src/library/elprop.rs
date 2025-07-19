82792eba0dc5231d971ea15535a41b3ba7a93b2b
Aleksei Gusev <aleksei.gusev@gmail.com>
2025-07-19 16:46:18 +0300

    Add documentation for `assert_elprop!`

--- src/library/elprop.rs
+++ src/library/elprop.rs
@@ -19,6 +19,35 @@ pub fn eval(lisp: &str) -> Result<String> {
     INFERIOR_EMACS.with_borrow_mut(|inf_emacs| inf_emacs.eval(lisp))
 }
 
+/// Asserts Elisp form evaluates identically in Emacs and Rune.
+///
+/// This macro is particularly useful for property-based testing, allowing
+/// you to ensure consistency between Emacs Lisp's behavior and Rune's
+/// interpretation across various inputs.
+///
+/// The Lisp `FORM` can contain placeholders for injecting Rust values, just like `format!`
+///
+/// ## Examples
+///
+/// Verify that the addition of 40 and 2 yields the same result in both Emacs and Rune:
+/// ```rust
+/// assert_elprop!["(+ 40 2)"];
+/// ```
+///
+/// Test that `make-vector` works consistently for any arbitrary byte size,
+/// demonstrating integration with property testing tools like `proptest`.
+/// Here, `size` is a Rust value injected into the Lisp form, while `?A`
+/// represents an arbitrary value managed internally by the macro for comparison:
+/// ```rust
+/// # use proptest::prelude::*;
+/// # fn arb_byte() -> impl Strategy<Value = u8> { any::<u8>() } // Mock `arb_byte` for example to compile
+/// proptest! {
+///     #[test]
+///     fn check_make_vector_consistency(size in arb_byte()) {
+///         assert_elprop!["(make-vector {} ?A)", size];
+///     }
+/// }
+/// ```
 #[macro_export]
 macro_rules! assert_elprop {
     ($($form:tt)*) => {{
